49b8b675ee62d9787a33803af76eb9ee46158978
MarceloZometa2198 <marcelo.zometa@hotmail.com>
2019-10-14 00:03:20 -0600

    Added a function to display the menu options

--- transaction.go
+++ transaction.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func displayMenu() {
+	fmt.Println("Greetings Marcelo\n")
+	fmt.Println("Welcome back to your Expense Tracker\n")
+	fmt.Println("What is it going to be for today?")
+	fmt.Println("[1]Get total incomes")
+	fmt.Println("[2]Add an income")
+	fmt.Println("[3]Get total outcomes")
+	fmt.Println("[4]Add an outcome")
+}
+
 func main() {
-  fmt.Println("Hello World")
+	displayMenu()
 }
