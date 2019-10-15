16b39d16ffbc28696db80b377c535a998a286e9d
MarceloZometa2198 <marcelo.zometa@hotmail.com>
2019-10-15 05:24:30 +0000

    Got to insert to the database new incomes

--- transaction.go
+++ transaction.go
@@ -3,87 +3,114 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	//"log"
+	"log"
 	_ "github.com/denisenkom/go-mssqldb"
 	"time"
+	"math"
+	"os"
+	"bufio"
   )
-// //Displays menu options. 
-// func displayMenu() {
-// 	fmt.Println("Greetings Marcelo\n")
-// 	fmt.Println("Welcome back to your Expense Tracker\n")
-// 	fmt.Println("What is it going to be for today?")
-// 	fmt.Println("[1]Get total incomes")
-// 	fmt.Println("[2]Add an income")
-// 	fmt.Println("[3]Get total outcomes")
-// 	fmt.Println("[4]Add an outcome")
-// 	fmt.Println("[5]Exit the program")
-// }
+//Displays menu options. 
+func displayMenu() {
+	fmt.Println("Greetings Marcelo\n")
+	fmt.Println("Welcome back to your Expense Tracker\n")
+	fmt.Println("What is it going to be for today?")
+	fmt.Println("[1]Get total incomes")
+	fmt.Println("[2]Add an income")
+	fmt.Println("[3]Get total outcomes")
+	fmt.Println("[4]Add an outcome")
+	fmt.Println("[5]Exit the program")
+}
 
-// //Gets the user input for what to do in Menu
-// func getChoice() int {
-// 	var num int
+//Gets the user input for what to do in Menu
+func getInt() int {
+	var num int
+	fmt.Scan(&num)	
+	return num
+}
 
-// 	return fmt.Scan(&num)	
-// }
+func getFloat() float64 {
+	var num float64
 
-// func menu(choice int) bool {
+	fmt.Scan(&num)	
+	num = math.Floor(num * 100) / 100
+
+	return num
+}
+
+func getString() string {
+	reader := bufio.NewReader(os.Stdin)
+	//var mystr string
+	mystr, _ := reader.ReadString('\n')
+	return mystr
+}
+
+func menu(choice int) bool {
 	
-// 	switch choice {
-// 		case 1:	//Get total incomes
-				
-// 		case 2: //Add an income
+	switch choice {
+		case 1:	//Get total incomes
+				//caseOne()
+		case 2: //Add an income
+				caseTwo()
+				break
+		case 3: //Get total outcomes
 
-// 		case 3: //Get total outcomes
+		case 4: // Add an outcome
 
-// 		case 4: // Add an outcome
+		case 5: //Exit the program
+				return false
+	}
 
-// 		case 5: //Exit the program
-// 				return false
-// 	}
+	return true
+}
 
-// 	return true
+// func caseOne() {
+// 	condb, errdb := sql.Open("mssql", "server=snow-se-1.snow.edu;user id=F19MarceloZometa;password=Password1!;")
+// 		if errdb != nil {
+// 			fmt.Println(" Error open db:", errdb.Error())
+// 		}
 // }
 
-//Driver function
-func main() {
-	//var choice int
-	//var runBack bool
+func caseTwo() {
+	//Creation of variables
 	var sqlStatement string
+	var money float64
+	var income string
+	id := 0
 
+	//Getting the info from user
+	fmt.Println("Input the name of this income: ")
+	income = getString()
+
+	fmt.Println("Input the amount of this income: ")
+	money = getFloat()
+
+	//Creating connection
 	condb, errdb := sql.Open("mssql", "server=snow-se-1.snow.edu;user id=F19MarceloZometa;password=Password1!;")
 		if errdb != nil {
 			fmt.Println(" Error open db:", errdb.Error())
 		}
-
-	// displayMenu()
-	// choice = getChoice()
-	
-	// runBack = menu(choice)
-	
+		
+	//Preparing query and executing it
 	sqlStatement = "Insert into F19MarceloZ.GoLangTransactions.Income (income, dateInput, amount) VALUES ($1, $2, $3);"
 
-	id := 0
-	money := 1000.26
-	errdb = condb.QueryRow(sqlStatement, "Jugar Origins", time.Now(), money).Scan(&id)
+	errdb = condb.QueryRow(sqlStatement, income, time.Now(), money).Scan(&id)
 	if errdb != nil {
-		panic(errdb)
+		log.Fatal(errdb)
 	  }
 	  fmt.Println("New record ID is:", id) 
-	
-	//   var (
-	// 	sqlversion string
-	//   )
-	//   rows, err := condb.Query("select @@version")
-	//   if err != nil {
-	// 	log.Fatal(err)
-	//   }
-	//   for rows.Next() {
-	// 	err := rows.Scan(&sqlversion)
-	// 	if err != nil {
-	// 	  log.Fatal(err)
-	// 	}
-	// 	log.Println(sqlversion)
-	//   }
+
 	defer condb.Close()
+}
+
+//Driver function
+func main() {
+	var choice int
+	//var runBack bool
 	
+	displayMenu()
+	choice = getInt()
+	
+	menu(choice)
+	//runBack = menu(choice)
 }
