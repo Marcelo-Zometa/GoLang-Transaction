4b4248c99a9ec53c695a5607c567f100eccebb7e
MarceloZometa2198 <marcelo.zometa@hotmail.com>
2019-10-14 19:05:48 -0600

    run the program by doing exactly what the guy did

--- transaction.go
+++ transaction.go
@@ -4,60 +4,81 @@ import (
 	"fmt"
 	"database/sql"
 	"github.com/denisenkom/go-mssqldb"
+	"log"
 )
 
 //Displays menu options. 
-func displayMenu() {
-	fmt.Println("Greetings Marcelo\n")
-	fmt.Println("Welcome back to your Expense Tracker\n")
-	fmt.Println("What is it going to be for today?")
-	fmt.Println("[1]Get total incomes")
-	fmt.Println("[2]Add an income")
-	fmt.Println("[3]Get total outcomes")
-	fmt.Println("[4]Add an outcome")
-	fmt.Println("[5]Exit the program")
-}
+// func displayMenu() {
+// 	fmt.Println("Greetings Marcelo\n")
+// 	fmt.Println("Welcome back to your Expense Tracker\n")
+// 	fmt.Println("What is it going to be for today?")
+// 	fmt.Println("[1]Get total incomes")
+// 	fmt.Println("[2]Add an income")
+// 	fmt.Println("[3]Get total outcomes")
+// 	fmt.Println("[4]Add an outcome")
+// 	fmt.Println("[5]Exit the program")
+// }
 
-//Gets the user input for what to do in Menu
-func getChoice() int {
-	var num int
+// //Gets the user input for what to do in Menu
+// func getChoice() int {
+// 	var num int
 
-	return fmt.Scan(&num)	
-}
+// 	return fmt.Scan(&num)	
+// }
 
-func menu(choice int) bool {
+// func menu(choice int) bool {
 	
-	switch choice {
-		case 1:	//Get total incomes
-				conn, err := sql.Open("mssql", "server=snow-se-1@snow.edu;userid=F19MarceloZometa;password=SA_PASSWORD=Password1!;")
-				if err == nil {
-					fmt.Println("It worked!")
-				}
-				else
-				{
-					fmt.Println("Connection failed")
-				}
+// 	switch choice {
+// 		case 1:	//Get total incomes
+// 				conn, err := sql.Open("mssql", "server=snow-se-1@snow.edu;userid=F19MarceloZometa;password=SA_PASSWORD=Password1!;")
+// 				if err == nil {
+// 					fmt.Println("It worked!")
+// 				}
+// 				else
+// 				{
+// 					fmt.Println("Connection failed")
+// 				}
 
-		case 2: //Add an income
+// 		case 2: //Add an income
 
-		case 3: //Get total outcomes
+// 		case 3: //Get total outcomes
 
-		case 4: // Add an outcome
+// 		case 4: // Add an outcome
 
-		case 5: //Exit the program
-				return false
-	}
+// 		case 5: //Exit the program
+// 				return false
+// 	}
 
-	return true
-}
+// 	return true
+// }
 
 //Driver function
 func main() {
-	var choice int
-	var runBack bool
+	// var choice int
+	// var runBack bool
 
-	displayMenu()
-	choice = getChoice()
+	// displayMenu()
+	// choice = getChoice()
 	
-	runBack = menu(choice)
+	// runBack = menu(choice)
+
+	condb, errdb := sql.Open("mssql", "server=localhost;user id=sa;password=SA_PASSWORD=yourStrong(!)Password;")
+	if errdb != nil {
+		fmt.Println(" Error open db:", errdb.Error())
+	}
+	var (
+		sqlversion string
+	)
+	rows, err := condb.Query("select @@version")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		err := rows.Scan(&sqlversion)
+		if err != nil {
+		log.Fatal(err)
+		}
+		log.Println(sqlversion)
+	}
+	defer condb.Close()
 }
