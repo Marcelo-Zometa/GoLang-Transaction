82cc0ad41209ebd624651e4b0348046520a88193
MarceloZometa2198 <marcelo.zometa@hotmail.com>
2019-10-15 04:47:33 +0000

    I can input to the database now

--- transaction.go
+++ transaction.go
@@ -3,10 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	//"log"
 	_ "github.com/denisenkom/go-mssqldb"
+	"time"
   )
-//Displays menu options. 
+// //Displays menu options. 
 // func displayMenu() {
 // 	fmt.Println("Greetings Marcelo\n")
 // 	fmt.Println("Welcome back to your Expense Tracker\n")
@@ -29,15 +30,7 @@ import (
 	
 // 	switch choice {
 // 		case 1:	//Get total incomes
-// 				conn, err := sql.Open("mssql", "server=snow-se-1@snow.edu;userid=F19MarceloZometa;password=SA_PASSWORD=Password1!;")
-// 				if err == nil {
-// 					fmt.Println("It worked!")
-// 				}
-// 				else
-// 				{
-// 					fmt.Println("Connection failed")
-// 				}
-
+				
 // 		case 2: //Add an income
 
 // 		case 3: //Get total outcomes
@@ -53,32 +46,44 @@ import (
 
 //Driver function
 func main() {
-	// var choice int
-	// var runBack bool
+	//var choice int
+	//var runBack bool
+	var sqlStatement string
+
+	condb, errdb := sql.Open("mssql", "server=snow-se-1.snow.edu;user id=F19MarceloZometa;password=Password1!;")
+		if errdb != nil {
+			fmt.Println(" Error open db:", errdb.Error())
+		}
 
 	// displayMenu()
 	// choice = getChoice()
 	
 	// runBack = menu(choice)
+	
+	sqlStatement = "Insert into F19MarceloZ.GoLangTransactions.Income (income, dateInput, amount) VALUES ($1, $2, $3);"
 
-	condb, errdb := sql.Open("mssql", "server=snow-se-1.snow.edu;user id=F19MarceloZometa;password=Password1!;")
+	id := 0
+	money := 1000.26
+	errdb = condb.QueryRow(sqlStatement, "Jugar Origins", time.Now(), money).Scan(&id)
 	if errdb != nil {
-		fmt.Println(" Error open db:", errdb.Error())
+		panic(errdb)
 	  }
-	  var (
-		sqlversion string
-	  )
-	  rows, err := condb.Query("select @@version")
-	  if err != nil {
-		log.Fatal(err)
-	  }
-	  for rows.Next() {
-		err := rows.Scan(&sqlversion)
-		if err != nil {
-		  log.Fatal(err)
-		}
-		log.Println(sqlversion)
-	  }
-	  defer condb.Close()
+	  fmt.Println("New record ID is:", id) 
+	
+	//   var (
+	// 	sqlversion string
+	//   )
+	//   rows, err := condb.Query("select @@version")
+	//   if err != nil {
+	// 	log.Fatal(err)
+	//   }
+	//   for rows.Next() {
+	// 	err := rows.Scan(&sqlversion)
+	// 	if err != nil {
+	// 	  log.Fatal(err)
+	// 	}
+	// 	log.Println(sqlversion)
+	//   }
+	defer condb.Close()
 	
 }
